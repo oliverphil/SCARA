89719e3d12f29545ec72ad4cc5e11ec9057e7271
oliverphil <oliverphil@myvuw.ac.nz>
2017-09-20 15:47:34 +1200

    Add files via upload

---
+++ kinematicsCheck.m
@@ -0,0 +1,36 @@
+function [ angleLeft, angleRight ] = kinematicsCheck(anglej12, anglej11, anglej22, anglej21)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+angleLeft = anglej12;
+if anglej12<anglej11
+    angleLeft = anglej11;
+end
+
+if anglej12>pi
+    angleLeft = anglej11;
+elseif anglej11>pi
+    angleLeft = anglej12
+end
+
+if angleLeft>pi
+    return;
+end
+
+angleRight = anglej22;
+if anglej22>anglej21
+    angleRight = anglej21;
+end
+
+if anglej22<0
+    angleRight = anglej21;
+elseif anglej21<0
+    angleRight = anglej22;
+    end
+
+if angleRight<0
+    return;
+end
+
+
+end
+
